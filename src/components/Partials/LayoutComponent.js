f5ade7ff595dc42c796f36fa797cb089bf19ef74
disaisoft <disaisoft@gmail.com>
2019-08-18 23:16:28 -0500

    se agrego nutricion fitness bienestar-cards-components

--- src/components/Partials/LayoutComponent.js
+++ src/components/Partials/LayoutComponent.js
@@ -5,6 +5,7 @@ import Footer from '../Partials/FooterComponent';
 import HomeComponent from '../HomeComponent/HomeComponent';
 import AboutMe from '../AboutMe/AboutMe';
 import Start from '../StartComponent/StartComponent';
+import Cards from '../CardsComponent/CardsComponent';
 
 //import Updates from '../UpdatesComponent/UpdatesComponent';
 //import HomeComponent from '../Home/HomeComponent';
@@ -17,7 +18,8 @@ const Layout = (props) => {
              {props.children}
              <HomeComponent/><br/>
              <AboutMe/><br/>
-             <Start/>
+             <Start/><br/>
+             <Cards/>
                
              
              <br/>
