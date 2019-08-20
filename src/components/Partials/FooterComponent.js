8df9f2eb82147fbb1232aa91c8f7c581b0e56272
disaisoft <disaisoft@gmail.com>
2019-08-20 14:08:35 -0500

    actualizacion del footer copyright

--- src/components/Partials/FooterComponent.js
+++ src/components/Partials/FooterComponent.js
@@ -11,6 +11,12 @@ class Footer extends React.Component {
               <div class="col"><a class="nav-link" href="a"><span>Facebook</span></a></div>
               <div class="col"><a class="nav-link" href="a"><span>Instagram</span></a></div>
               <div class="col"><a class="nav-link" href="a"><span>Twitter</span></a></div>
+            </div><br />
+            <div class="text-center">
+              <p class="color-footer">&copy; Copyright <strong>David I. López</strong> Todos Los Derechos Reservados</p>
+              <div class="credits">
+                Diseñado Por <a href="#">David I. López F.</a>
+              </div>
             </div>
           </footer>
         );
